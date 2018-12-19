f565a18e994d0c92b0708f12e50313db47016c30
NullDev (Shadow) <shadow@nulldev.org>
2018-12-19 16:23:09 +0100

    output kernel name

--- boot_sector.asm
+++ boot_sector.asm
@@ -1,5 +1,17 @@
-loop:
-    jmp loop 
+mov ah, 0x0e
+
+mov al, 'S'
+int 0x10
+mov al, 'h'
+int 0x10
+mov al, 'i'
+int 0x10
+mov al, 'r'
+int 0x10
+mov al, 'o'
+int 0x10
+
+jmp $
 
 times 510-($-$$) db 0
 dw 0xaa55 
