f2ad259f1e3f0447f22d2ef784911c85fad61e58
CreepSore <creepsore@gmail.com>
2019-02-22 00:16:41 +0100

    Fixed 32-Bit Printing with much love <3

--- init32/32bit_print.asm
+++ init32/32bit_print.asm
@@ -3,9 +3,24 @@
 VIDEO_MEMORY equ 0xb8000
 WHITE_ON_BLACK equ 0x0f
 
+LINE_OFFSET db 0xA0 ; Magic
+OFFSET_COUNT db 0x03 ; Start at the third line
+
 print_string_pm:
     pusha
     mov edx, VIDEO_MEMORY
+
+print_string_pm_calcOffset:
+    push eax
+    push ebx
+
+    movzx eax, byte [LINE_OFFSET]
+    movzx ebx, byte [OFFSET_COUNT]
+    imul eax, ebx
+    add edx, eax
+
+    pop ebx
+    pop eax
 
 print_string_pm_loop:
     mov al, [ebx]
@@ -21,5 +36,6 @@ print_string_pm_loop:
     jmp print_string_pm_loop
 
 print_string_pm_done:
+    inc byte [OFFSET_COUNT]
     popa
     ret
