8654cd602cf60f0ab989bf1ee5ad40f36a5209f8
CreepSore <creepsore@gmail.com>
2019-02-26 22:57:12 +0100

    Fixed and compressed setchar

--- init32/32bit_setchar.asm
+++ init32/32bit_setchar.asm
@@ -1,29 +1,27 @@
 [bits 32]
 
-; esp+4 = char
-; esp+8 = color
-; esp+12 = x
-; esp+16 = y
+; ebx+0 = char
+; ebx+1 = color
+; ebx+2 = x
+; ebx+3 = y
 
 setchar:
 .setchar_begin:
     pusha
-    mov ecx, LINE_WIDTH
-    mov eax, [ebx+12]
-    mov edx, [ebx+16]
-    imul edx, ecx         ; Multiply Y-Offset by Screen-Width
-    add eax, edx          ; Add Y-Offset to X-Offset
-    imul eax, 2
+
+.setchar_calcoffset:
+    movzx edx, byte [ebx+2]
+    movzx eax, byte [ebx+3]
+    imul edx, 2             ; Multiply by 2 because of Color-Bytes
+    imul eax, LINE_WIDTH    ; Multiply Y-Offset by Screen-Width
+    add edx, eax            ; Add Y-Offset to X-Offset
 
 .setchar_main:
-    mov edx, VIDEO_MEMORY
-    add edx, eax
+    add edx, VIDEO_MEMORY   ; Add Video-Memory-Offset
 
-    mov ah, byte [ebx+8]
-    mov al, byte [ebx+4]
+    mov ax, word [ebx]
     mov [edx], ax
 
 .setchar_end:
     popa
     ret
-
