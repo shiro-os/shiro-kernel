0109037e489fc89b774ffdde7e971e6c1b6dead9
CreepSore <creepsore@gmail.com>
2019-12-08 14:35:05 +0100

    [Needs further Testing] Fixed Drive Error

--- bootsector/bootsector_main.asm
+++ bootsector/bootsector_main.asm
@@ -5,7 +5,6 @@
     mov bp, 0x9000
     mov sp, bp
 
-    call cls
     call load_kernel
     call switch_to_pm
 
@@ -13,7 +12,7 @@
 
 %include "bootsector/constants.asm"
 %include "bootsector/bootsector_disk.asm"
-%include "bootsector/bootsector_cls.asm"
+%include "bootsector/bootsector_print.asm"
 %include "init32/32bit_gdt.asm"
 %include "init32/32bit_print.asm"
 %include "init32/32bit_switch.asm"
@@ -24,7 +23,7 @@ LOAD:
 [bits 16]
     load_kernel:
         mov bx, KERNEL_OFFSET
-        mov dh, 2
+        mov dh, 1
         mov dl, [BOOT_DRIVE]
         call disk_load
         ret
@@ -42,7 +41,7 @@ LOAD:
 BOOT_DRIVE db 0
 
 MSG_LOAD_KERNEL db "[INFO]  - Loading kernel into memory", 0
-MSG_EXEC_KERNEL db "[INFO] - Kernel execution was finished", 0
+MSG_EXEC_KERNEL db "[INFO]  - Kernel execution was finished", 0
 
 times 510-($-$$) db 0
 dw 0xaa55
