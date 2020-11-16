84f177ff3843a09cfc1fb4c4d92a67e0c513952e
ehdes <ehdes@protonmail.com>
2020-11-16 23:14:59 +0100

    Added temporary auto-compiler

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 function cleanup() {
     printf "\e[33m (Re)creating binary folder \e[0m\n"
     rm -r ./bin
@@ -11,28 +10,37 @@ function cleanup() {
 CROSS_COMPILER_PATH=$(which i686-elf-g++)
 
 function compileKernel() {
-    set -e
+    toCompile=("./kernel/kernel.cpp" "./kernel/Terminal.cpp" "./kernel/math.c" "./kernel/util.c" "./kernel/condraw.c")
+    outputs=()
     printf "\e[33m Compiling Kernel... [step: compiling; file: kernel_ep.asm] \e[0m\n"
     # -f: Format, compile as elf64 image so we can merge the header with our C Kernel
     nasm -f elf32  ./kernel/kernel_ep.asm -o ./bin/boot.o
     
     if [ -x "$CROSS_COMPILER_PATH" ]; then
         printf "\e[33m Detected Cross Compiler... going forward with i686-elf-g++ \e[0m\n"
-        printf "\e[33m Building File for Shiro: kernel.cpp... \e[0m\n"
-        i686-elf-g++ -c ./kernel/kernel.cpp -o ./bin/kernel.o -ffreestanding -fno-exceptions -fno-rtti
-        printf "\e[33m Building File for Shiro: Terminal.cpp... \e[0m\n"
-        i686-elf-g++ -c ./kernel/Terminal.cpp -o ./bin/Terminal.o -ffreestanding -fno-exceptions -fno-rtti
+
+        for i in "${toCompile[@]}"; do
+            printf "\e[33m Building File: $i... \e[0m\n"
+            outputName=$(echo $(basename -- "$i") | sed "s/\(.*\)\(\..*\)/\.\/bin\/\1.o/g")
+            outputs+=($outputName)
+            i686-elf-g++ -c $i -o $o -ffreestanding -fno-exceptions -fno-rtti
+        done
     else
         printf "\e[33m Missing correct Cross compiler... Fallback to g++ (May cause unwanted behaviour) \e[0m\n"
-        printf "\e[33m Building File: kernel.cpp... \e[0m\n"
-        g++ -m32 -c ./kernel/kernel.cpp -o ./bin/kernel.o -ffreestanding -fno-exceptions -fno-rtti
+
+        for i in "${toCompile[@]}"; do
+            printf "\e[33m Building File: $i... \e[0m\n"
+            outputName=$(echo $(basename -- "$i") | sed "s/\(.*\)\(\..*\)/\.\/bin\/\1.o/g")
+            outputs+=($outputName)
+            g++ -m32 -c $i -o $outputName -ffreestanding -fno-exceptions -fno-rtti
+        done
     fi
 
     printf "\e[33m Linking binaries... \e[0m\n"
     if [ -x "$CROSS_COMPILER_PATH" ]; then
-        i686-elf-gcc ./bin/boot.o ./bin/kernel.o ./bin/Terminal.o -T link.ld -o./bin/shiro.bin -nostdlib -nodefaultlibs -lgcc
+        i686-elf-gcc ./bin/boot.o ${outputs[@]} -T link.ld -o./bin/shiro.bin -nostdlib -nodefaultlibs -lgcc
     else
-        gcc -m32 ./bin/boot.o ./bin/kernel.o ./bin/Terminal.o -T link.ld -o./bin/shiro.bin -nostdlib -nodefaultlibs
+        gcc -m32 ./bin/boot.o ${outputs[@]} -T link.ld -o./bin/shiro.bin -nostdlib -nodefaultlibs
     fi
 }
 
@@ -53,6 +61,11 @@ function createImage() {
 function launch() {
     printf "\e[33m Launching VM... \e[0m\n"
     qemu-system-i386 -cdrom ./bin/shiro.iso
+}
+
+function launchDbg() {
+    printf "\e[33m Launching VM in Debug mode... \e[0m\n"
+    qemu-system-i386 -m 1024 -cdrom ./bin/shiro.iso -s -S
 }
 
 
